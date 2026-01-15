d3b1292235c20ae1bafaf40e052720c83997ac8c
jayasankahws-glitch <jayasankahws@gmail.com>
2026-01-15 11:12:45 +0530

    Add hello.py file

---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Hello Guys! Welcome to my GitHub repository.")
+print("I am Jayasanka, an undergraduate BSc IT student at Sri Lanka Open University")
+print("This Python script is created for an academic assignment.")
